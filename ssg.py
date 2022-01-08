4427df06dd2976179486f5cde2009736afbe38c2
ELSchnorro <simonknehr@gmail.com>
2022-01-08 16:41:17 +0100

    Done

---
+++ ssg.py
@@ -0,0 +1,12 @@
+import typer
+from ssg.site import site
+
+def main(source = "content", dest = "dist"):
+    configure = {"source":source, "dest":dest}
+    site(**configure).build()
+
+typer.run(main)
+
+
+
+   
